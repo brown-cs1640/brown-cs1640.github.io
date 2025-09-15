49a97fd633a2f7081d4407bc002bc441336ca1ca
kerumeto <117316944+kerumeto@users.noreply.github.com>
2025-09-15 14:36:47 -0400

    calendar fixes

--- website/src/Calendar/Calendar.tsx
+++ website/src/Calendar/Calendar.tsx
@@ -9,7 +9,7 @@ export default function Calendar() {
       <h3>Calendar</h3>
       <Container className="frame-container">
         <iframe
-          src="https://calendar.google.com/calendar/u/0?cid=Y183OGZiNTg4YTdhYWZmMWEwMTNkNmUxYjY2MjRhMmU1NjEyOWEzYzllZDZmMjVhMmU4ZTVkNGQxNmExODczZDJiQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20"
+          src="https://calendar.google.com/calendar/embed?src=c_78fb588a7aaff1a013d6e1b6624a2e56129a3c9ed6f25a2e8e5d4d16a1873d2b%40group.calendar.google.com&ctz=America%2FDenver"
           style={{ border: 0 }}
           width="100%"
           height="100%"
